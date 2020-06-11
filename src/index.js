2217c3b590a2c50661332fcf817764001090144e
Toni Peise <toni.peise@gmx.net>
2020-06-12 01:46:53 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-//Import .js here import "./js/";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-console.log("index.js is working");
+gsap.registerPlugin(ScrollTrigger);
+
+let tl = gsap.timeline({
+   scrollTrigger: {
+      trigger: ".dark",
+      start: "top bottom",
+      scrub: true,
+      anticipatePin: 1,
+   },
+});
+
+tl.from("img", { x: 200, opacity: 0, duration: 1 }).from(
+   ".content",
+   {
+      y: 300,
+
+      duration: 1,
+   },
+   "-=0.5"
+);
